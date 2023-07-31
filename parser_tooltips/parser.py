959e7081e66d7908dbc4b071c584ae55ab2d47c8
Nina Resch <reschni96@gmail.com>
2023-07-31 19:51:40 +0200

    Tooltip fixes

--- parser_tooltips/parser.py
+++ parser_tooltips/parser.py
@@ -2,9 +2,16 @@ def parse_file(input_file, output_file):
     with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', encoding='utf-8') as outfile:
         lines = infile.read().split('\n')
 
+        outfile.write("var tooltipList = [\n")
+
         for i in range(0, len(lines), 3):  # skip 3 lines (phrase, explanation, and empty line)
-            phrase = lines[i]
-            explanation = lines[i+1] if i+1 < len(lines) else ""
-            outfile.write(f'replaceWithTooltip(document.body, "{phrase}", "{explanation}");\n')
+            phrase = lines[i].replace('"', '\\"')  # Escape any double quotes
+            explanation = lines[i + 1].replace('"', '\\"') if i + 1 < len(lines) else ""
+            outfile.write(f'    {{searchString: "{phrase}", explanationText: "{explanation}"}},\n')
+
+        outfile.write("];\n")
+
+        outfile.write("replaceWithTooltip(document.body, tooltipList);\n")
+
 
 parse_file('input.txt', 'output.txt')
